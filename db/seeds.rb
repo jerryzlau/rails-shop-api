71ab46c1603e654da246da816402dbc1c6b09731
Jerry Lau <jerryzlau@gmail.com>
2017-12-13 23:23:43 -0800

    baseline model require accomplished, inventory and index model seem to be needed for full shopping experience

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,17 @@
 require 'faker'
 
 # users
-10.times do |n|
+5.times do |n|
   User.create(first_name: Faker::Name.unique.first_name,
               last_name: Faker::Name.unique.last_name)
 end 
 
-# Categories 
+# Order
+Order.create(
+  customer_id: 1
+)
+
+# Category
 Category.create(name: 'snacks')
 Category.create(name: 'chips')
 Category.create(name: 'crackers')
@@ -23,25 +28,58 @@ Category.create(name: 'breakfast')
 Category.create(name: 'bars')
 Category.create(name: 'cereal')
 
-# order
-Order.create(
-  customer_id: 1
-)
-
-# food products 
-# inventory (without order_id means not bought)
+# Product weight in oz
+# Order 1 Product 1
 Product.create(name: 'Doritos Nacho Cheese Flavored Tortilla Chips, Party Size!',
   price: 4.77,
   weight: 15,
-  category_id: 1,
+  order_id: 1
+) 
+
+# Order 1 Product 2
+Product.create(name: 'Doritos Nacho Cheese Flavored Tortilla Chips, Party Size!',
+  price: 3.64,
+  weight: 21,
+  order_id: 1
+) 
+
+# Order 1 Product 3
+Product.create(name: 'Pringles 2 Flavor Snack Stacks, 0.63 Ounce, 18 count',
+  price: 12.50,
+  weight: 0.63,
   order_id: 1
 )
 
-Product.create(name: 'Doritos Nacho Cheese Flavored Tortilla Chips, Party Size!',
-  price: 4.77,
-  weight: 15,
+# Product Category 
+# Chips
+ProductCategory.create(
+  category_id: 1,
+  product_id: 1
+)
+ProductCategory.create(
   category_id: 2,
-  order_id: 1)
+  product_id: 1
+)
+
+ProductCategory.create(
+  category_id: 1,
+  product_id: 3
+)
+ProductCategory.create(
+  category_id: 2,
+  product_id: 3
+)
+
+# Cereal
+ProductCategory.create(
+  category_id: 4,
+  product_id: 2
+)
+ProductCategory.create(
+  category_id: 6,
+  product_id: 2
+)
+
 
 
 
